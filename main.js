6f1bfd842871073c73122723aa1a30761fce5001
Odinachi <odinachidavid@yahoo.com>
2021-02-23 00:17:01 +0100

    basic setup

---
+++ main.js
@@ -0,0 +1,36 @@
+SHA256 = require('crypto-js/sha256')
+
+
+class Block {
+    constructor(index, timestamp, data,  previousHash ='') {
+        this.index = index
+        this.timestamp = timestamp
+        this.data = data
+        this.previousHash = previousHash
+        this.hash = this.calculateHash() ;
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock]
+    }
+
+    createGenesisBlock (){
+        return new Block(0, "01/03/2022", 'Genesis Block', 0)
+    }
+
+    getLatestBlock() {
+        this.chain[this.chain.lenght - 1]
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash
+        newBlock.hash = newBlock.calculateHash()
+        this.chain.push(newBlock)
+    }
+}
