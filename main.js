f15f9fd1218a16b8d629f131195ca68d2488b252
Odinachi <odinachidavid@yahoo.com>
2021-02-23 01:15:33 +0100

    after adding proof of work

--- main.js
+++ main.js
@@ -7,21 +7,30 @@ class Block {
         this.timestamp = timestamp
         this.data = data
         this.previousHash = previousHash
-        this.hash = this.calculateHash() ;
+        this.hash = this.calculateHash()
+        this.nonce = 0
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString()
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++
+            this.hash=this.calculateHash()
+        }
     }
 }
 
 class Blockchain {
     constructor() {
-        this.chain = [this.createGenesisBlock]
+        this.chain = [this.createGenesisBlock()]
+        this.difficulty = 10
     }
 
     createGenesisBlock (){
-        return new Block(0, "01/03/2022", 'Genesis Block', 0)
+        return new Block(0, "01/03/2022", 'Genesis Block', '0')
     }
 
     getLatestBlock() {
@@ -29,8 +38,31 @@ class Blockchain {
     }
 
     addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash
-        newBlock.hash = newBlock.calculateHash()
-        this.chain.push(newBlock)
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty)
+        this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for(let i =1; i < this.chain.length; i++) {
+            const currentBlock= this.chain(i)
+            const previousBlock = this.chain(i-1)
+
+            if(currentBlock !== currentBlock.calculateHash()) {
+                return false
+            }
+            
+            if(previousBlock.previousHash !== previousBlock.hash){
+                return false
+            }
+
+        }
+
+        return true
     }
 }
+
+let odicoin = new Blockchain()
+odicoin.addBlock(new Block(1, "20/2/2022", {amount: 8}))
+odicoin.addBlock(new Block(1, "20/2/2023", {amount: 12}))
+odicoin.addBlock(new Block(1, "20/2/2024", {amount: 80}))
